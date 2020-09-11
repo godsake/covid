fc9c1eb1785068fc788f3b4d3852f51ef4df0cf9
Sebastien-Proulx <sebas_proulx@hotmail.com>
2020-09-11 17:02:46 -0400

    first commit

---
+++ src/test/java/com/seb/covid/CovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
