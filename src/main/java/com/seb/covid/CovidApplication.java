a1be4e7aba26d04c8dd7a6c9333c97f63058c16a
Sebastien-Proulx <sebas_proulx@hotmail.com>
2020-09-13 12:55:12 -0400

    factory

--- src/main/java/com/seb/covid/CovidApplication.java
+++ src/main/java/com/seb/covid/CovidApplication.java
@@ -3,21 +3,11 @@ package com.seb.covid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 @SpringBootApplication
-@Service
 public class CovidApplication {
 
 	public static void main( String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
-			"META-INF/beans.xml");
-		BeanFactory factory = context;
-		ICovidService covidService = (ICovidService) factory.getBean("covidService");
-
+		ICovidService covidService = ServiceFactory.getCovidService();
 		System.out.println("RES: " + covidService.getCovidResult(
 			CovidRegionEnum.CENTRE_QUEBEC).getNbrDeath());
 
---
+++ src/main/java/com/seb/covid/ServiceFactory.java
@@ -0,0 +1,18 @@
+package com.seb.covid;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@Service
+public class ServiceFactory {
+    public static ICovidService getCovidService() {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+			"META-INF/beans.xml");
+		BeanFactory factory = context;
+        ICovidService covidService = (ICovidService) 
+                                        factory.getBean("covidService");
+        return covidService;
+    }
+}
