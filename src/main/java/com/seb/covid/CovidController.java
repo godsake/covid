ec58904d8b557dc7b60e50c1a3164b33deb7e354
Sebastien-Proulx <sebas_proulx@hotmail.com>
2020-09-13 19:44:57 -0400

    liste des regions

--- src/main/java/com/seb/covid/CovidController.java
+++ src/main/java/com/seb/covid/CovidController.java
@@ -13,6 +13,28 @@ public class CovidController {
 
     @RequestMapping("/covid")
 	public String index(String region) throws Exception {
+
+        if (region == null){
+            return "<a href='/covid?region=Capitale-Nationale'>Capitale-Nationale</a><br/>"
+            + "<a href='/covid?region=Bas-Saint-Laurent'>Bas-Saint-Laurent</a><br/>"
+            + "<a href='/covid?region=Saguenay-Lac-Saint-Jean'>Saguenay-Lac-Saint-Jean</a><br/>"
+            + "<a href='/covid?region=Mauricie-et-Centre-du-Québec'>Mauricie-et-Centre-du-Québec</a><br/>"
+            + "<a href='/covid?region=Estrie'>Estrie</a><br/>"
+            + "<a href='/covid?region=Montréal'>Montréal</a><br/>"
+            + "<a href='/covid?region=Outaouais'>Outaouais</a><br/>"
+            + "<a href='/covid?region=Abitibi-Témiscamingue'>Abitibi-Témiscamingue</a><br/>"
+            + "<a href='/covid?region=Côte-Nord'>Côte-Nord</a><br/>"
+            + "<a href='/covid?region=Nord-du-Québec'>Nord-du-Québec</a><br/>"
+            + "<a href='/covid?region=Gaspésie-Îles-de-la-Madeleine'>Gaspésie-Îles-de-la-Madeleine</a><br/>"
+            + "<a href='/covid?region=Chaudière-Appalaches'>Chaudière-Appalaches</a><br/>"
+            + "<a href='/covid?region=Laval'>Laval</a><br/>"
+            + "<a href='/covid?region=Lanaudière'>Lanaudière</a><br/>"
+            + "<a href='/covid?region=Laurentides'>Laurentides</a><br/>"
+            + "<a href='/covid?region=Montérégie'>Montérégie</a><br/>"
+            + "<a href='/covid?region='></a><br/>";
+        }
+
+
         CovidResult result = covidService.getCovidResult(region);
 
         return region + ": Cas: " + result.getNbrInfected() + 
