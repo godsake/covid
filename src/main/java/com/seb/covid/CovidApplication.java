1475bcb15372dbcb0ddd148231a91ef2ce01474b
Sebastien-Proulx <sebas_proulx@hotmail.com>
2020-09-13 09:05:10 -0400

    aa

--- src/main/java/com/seb/covid/CovidApplication.java
+++ src/main/java/com/seb/covid/CovidApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CovidApplication {
 
-	public static void main(String[] args) {
+	public static void main( String[] args) {
 		SpringApplication.run(CovidApplication.class, args);
 	}
 
