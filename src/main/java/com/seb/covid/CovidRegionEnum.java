41785e4b6463a588d4a827d10ee3ac18deb8adc4
Sebastien-Proulx <sebas_proulx@hotmail.com>
2020-09-13 17:05:41 -0400

    All QC regions

--- src/main/java/com/seb/covid/CovidRegionEnum.java
+++ src/main/java/com/seb/covid/CovidRegionEnum.java
@@ -4,6 +4,11 @@ public enum CovidRegionEnum {
     CapitaleNationale("Capitale-Nationale"), 
     BasSaintLaurent("Bas-Saint-Laurent");
 
+    // Saguenay-Lac-Saint-Jean, Mauricie-et-Centre-du-Québec, Estrie, Montréal, 
+    // Outaouais, Abitibi-Témiscamingue, Côte-Nord, Nord-du-Québec, 
+    // Gaspésie-Îles-de-la-Madeleine, Chaudière-Appalaches, Laval,
+    // Lanaudière, Laurentides, Montérégie
+
     private String value;
 
     CovidRegionEnum(final String value) {
